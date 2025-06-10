521a169f64da33e27584eddf252365a775d54806
Evas122 <108418453+Evas122@users.noreply.github.com>
2025-06-10 21:24:01 +0200

    Initial create

---
+++ src/AddressBook.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The general information about the set is controlled by the following 
+// set of attributes. Change the values of these attributes to modify the information
+// associated with the set.
+[assembly: AssemblyTitle("AdressBook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdressBook")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting the ComVisible component to false makes the types in this set invisible
+// to COM components. If you need to access a type in this set from the
+// COM, set the value true for the ComVisible attribute of that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is the identifier of the type library when this project is made available in the COM model
+[assembly: Guid("750c0b35-b5ab-4243-a33d-caed001f4158")]
+
+// Set version information includes the following four values:
+//
+// Main version
+// Secondary version
+// Compilation number
+// Correction
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
